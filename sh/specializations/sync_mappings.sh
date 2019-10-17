47d5db89e1149767fefb4ab101d3d0d8359cf43d
Mark A. Greenslade <mark.greenslade@ieee.org>
2019-10-17 14:42:55 +0200

    1. Updating specialization related commands.

--- sh/specializations/sync_mappings.sh
+++ sh/specializations/sync_mappings.sh
@@ -3,10 +3,10 @@
 # Main entry point.
 main()
 {
-	rm -rf $ESDOC_DIR_BASH/cmip6/models/init_output/csv-files/*.csv
+	rm -rf $CMIP6_LIB/models/init_json_from_cmip5/csv-files/*.csv
 	for specialization in "${CMIP6_REALM_SPECIALIZATIONS[@]}"
 	do
-		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/mappings/*.csv $ESDOC_DIR_BASH/cmip6/models/init_output/csv-files
+		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/mappings/*.csv $CMIP6_LIB/models/init_json_from_cmip5/csv-files
 	done
 }
 
