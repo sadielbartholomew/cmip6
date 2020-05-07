31e6a0a597cba42ba6381377de1b4b6b448ab24b
Mark A. Greenslade <mark@casperlabs.io>
2020-05-07 13:37:46 +0200

    1. Latest wcrp vocabs. 2. Updated institutional gh repo verifier. 3. Updated model scripts.

---
+++ sh/institutes/github_repos_status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Main entry point.
+main()
+{
+	for institution_id in "${INSTITUTION_ID[@]}"
+	do
+		log "GITHUB : pulling  "$institution_id
+		cd $ESDOC_HOME/repos/institutional/$institution_id
+		git status
+	done
+}
+
+# Invoke entry point.
+main
