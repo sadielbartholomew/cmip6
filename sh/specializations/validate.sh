a73779e77aad0b085b172a1a481cf443709b3d23
Mark A. Greenslade <mark@casperlabs.io>
2021-03-29 11:43:01 +0200

    1. Updated specialisation validation.

--- sh/specializations/validate.sh
+++ sh/specializations/validate.sh
@@ -3,15 +3,31 @@
 # Main entry point.
 main()
 {
+	local PATH_TO_REPO
+	local REPO_NAME
+	local SPECIALIZATION
+
 	if [ "$1" ]; then
-		declare specialization=$1
-		pipenv run python "$CMIP6_HOME"/repos/specializations/cmip6-specializations-$specialization/validate
+		_validate "$1"
 	else
-		for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
+		for SPECIALIZATION in "${CMIP6_SPECIALIZATIONS[@]}"
 		do
-			pipenv run python "$CMIP6_HOME"/repos/specializations/cmip6-specializations-$specialization/validate
+			_validate "$SPECIALIZATION"
 		done
 	fi
+}
+
+function _validate()
+{
+	local PATH_TO_REPO
+	local REPO_NAME
+	local SPECIALIZATION=${1}
+
+	REPO_NAME=cmip6-specializations-"$SPECIALIZATION"
+	PATH_TO_REPO=$(get_path_to_repo "specializations" "$REPO_NAME")
+	pushd $CMIP6_HOME
+	pipenv run python "$PATH_TO_REPO"/validate
+	popd
 }
 
 # Invoke entry point.
